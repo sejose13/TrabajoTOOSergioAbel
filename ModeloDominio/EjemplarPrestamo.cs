dde61efa7b43be3cb26d8677d2a2f955768c9e0c
abhidalg <santabel2004@gmail.com>
2025-12-11 14:22:41 +0100

    sebe moñas

--- ModeloDominio/EjemplarPrestamo.cs
+++ ModeloDominio/EjemplarPrestamo.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace ModeloDominio
 {
-    internal class Class1
+    public class EjemplarPrestamo : IEquatable<EjemplarPrestamo>
     {
+        private Ejemplar ejemplar;
+        private Prestamo prestamo;
+        private DateTime fechaDevolucion;
+        private DateTime fechaPrevista;
+        private bool devuelto;
+
+        public EjemplarPrestamo(Ejemplar e, Prestamo p, DateTime fechaPrevista)
+        {
+            this.ejemplar = e;
+            this.prestamo = p;
+            this.fechaPrevista = fechaPrevista;
+            this.devuelto = false;
+        }
+
+        public Ejemplar Ejemplar
+        {
+            get { return ejemplar; }
+        }
+
+        public Prestamo Prestamo
+        {
+            get { return prestamo; }
+        }
+
+        public DateTime FechaDevolucion
+        {
+            get { return fechaDevolucion; }
+            set { fechaDevolucion = value; }
+        }
+
+        public bool Devuelto
+        {
+            get { return devuelto; }
+            set { devuelto = value; }
+        }
+
+        public bool Equals(EjemplarPrestamo other)
+        {
+            throw new NotImplementedException();
+        }
+        //anormal
     }
 }
