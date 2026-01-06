5a8bec8abbf395849ad53312430b2521e1edbd04
Sergio <sergiosanjosemartin@gmail.com>
2026-01-06 21:52:38 +0100

    xdnt

--- InterfazUsuario/FormDetalleUsuario.Designer.cs
+++ InterfazUsuario/FormDetalleUsuario.Designer.cs
@@ -28,55 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAceptar
             // 
-            this.button1.Location = new System.Drawing.Point(89, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAceptar.Location = new System.Drawing.Point(49, 119);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnCancelar
             // 
-            this.button2.Location = new System.Drawing.Point(227, 188);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCancelar.Location = new System.Drawing.Point(161, 119);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Location = new System.Drawing.Point(57, 43);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(29, 13);
+            this.txt.TabIndex = 2;
+            this.txt.Text = "DNI:";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(136, 40);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(89, 72);
+            this.label1.Location = new System.Drawing.Point(57, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre:";
             // 
-            // textBox1
+            // txtNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(189, 72);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtNombre.Location = new System.Drawing.Point(136, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
             // 
             // FormDetalleUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 316);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(315, 187);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
             this.Name = "FormDetalleUsuario";
             this.Text = "FormDetalleUsuario";
             this.ResumeLayout(false);
@@ -86,9 +106,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label txt;
+        private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNombre;
     }
 }
--- InterfazUsuario/FormDetalleUsuario.cs
+++ InterfazUsuario/FormDetalleUsuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LogicaNegocio;
+using ModeloDominio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,90 @@ namespace InterfazUsuario
 {
     public partial class FormDetalleUsuario : Form
     {
-        public FormDetalleUsuario()
+    
+ 
+        private LNPersonal _ln;
+        private Usuario _usuario;
+        private bool _modoAlta; // true = Alta, false = Baja o Consulta
+
+        
+        public FormDetalleUsuario(LNPersonal ln, Usuario usuario, bool modoAlta)
         {
             InitializeComponent();
+            _ln = ln;
+            _usuario = usuario;
+            _modoAlta = modoAlta;
+        }
+
+        private void FormDetalleUsuario_Load(object sender, EventArgs e)
+        {
+            // Rellenamos los campos
+            txtDni.Text = _usuario.Dni;
+            txtDni.Enabled = false; // El DNI no se toca, viene de la búsqueda anterior
+
+            if (_modoAlta)
+            {
+                // MODO ALTA: Campos vacíos y habilitados para escribir
+                this.Text = "Alta de usuario";
+                txtNombre.Text = "";
+                txtNombre.Enabled = true;
+            }
+            else
+            {
+                // MODO BAJA/CONSULTA: Campos rellenos y deshabilitados
+                this.Text = "Datos de usuario"; // o "Baja de usuario"
+                txtNombre.Text = _usuario.Nombre;
+                txtNombre.Enabled = false; // Solo lectura
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_modoAlta)
+                {
+                    // --- LÓGICA DE ALTA ---
+                    if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                    {
+                        MessageBox.Show("Debes introducir un nombre.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+       
+                    _usuario.Nombre = txtNombre.Text;
+
+                     _ln.DarAltaUsuario(_usuario);
+
+                    MessageBox.Show("Usuario creado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // --- LÓGICA DE BAJA ---
+                    // Si no es alta, asumimos que es Baja (o podríamos bloquear el botón si es consulta)
+                    DialogResult res = MessageBox.Show("¿Está seguro que desea dar de baja al usuario?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (res == DialogResult.Yes)
+                    {
+                        _ln.DarBajaUsuario(_usuario);
+                        MessageBox.Show("Usuario eliminado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        return; // Si dice que no, no cerramos la ventana
+                    }
+                }
+
+                this.Close(); // Cerramos al terminar
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
