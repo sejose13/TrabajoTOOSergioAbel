20301120e6e7a6699049c1c4d0c1d9d0ac21e846
Sergio <sergiosanjosemartin@gmail.com>
2026-01-07 02:01:07 +0100

    rr

---
+++ InterfazUsuario/FormDocumentoMasLeido.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace InterfazUsuario
+{
+    partial class FormDocumentoMasLeido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtEditorial = new System.Windows.Forms.TextBox();
+            this.txtIsbn = new System.Windows.Forms.TextBox();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "El documento del mes es:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(22, 52);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.ReadOnly = true;
+            this.txtTitulo.Size = new System.Drawing.Size(100, 20);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(143, 52);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.ReadOnly = true;
+            this.txtAutor.Size = new System.Drawing.Size(100, 20);
+            this.txtAutor.TabIndex = 2;
+            // 
+            // txtEditorial
+            // 
+            this.txtEditorial.Location = new System.Drawing.Point(22, 98);
+            this.txtEditorial.Name = "txtEditorial";
+            this.txtEditorial.ReadOnly = true;
+            this.txtEditorial.Size = new System.Drawing.Size(100, 20);
+            this.txtEditorial.TabIndex = 3;
+            // 
+            // txtIsbn
+            // 
+            this.txtIsbn.Location = new System.Drawing.Point(143, 98);
+            this.txtIsbn.Name = "txtIsbn";
+            this.txtIsbn.ReadOnly = true;
+            this.txtIsbn.Size = new System.Drawing.Size(100, 20);
+            this.txtIsbn.TabIndex = 4;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(96, 147);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // FormDocumentoMasLeido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 188);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.txtIsbn);
+            this.Controls.Add(this.txtEditorial);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormDocumentoMasLeido";
+            this.Text = "Documento más prestado del mes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtEditorial;
+        private System.Windows.Forms.TextBox txtIsbn;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
