92eb8d642806492668846a64b40fd7976b5dce54
Sergio <sergiosanjosemartin@gmail.com>
2026-01-07 02:08:20 +0100

    hhh

--- InterfazUsuario/FormDocumentoMasLeido.cs
+++ InterfazUsuario/FormDocumentoMasLeido.cs
@@ -1,4 +1,5 @@
-﻿using ModeloDominio;
+﻿using LogicaNegocio;
+using ModeloDominio;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,32 +10,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace InterfazUsuario
 {
     public partial class FormDocumentoMasLeido : Form
     {
-        private Documento _doc;
+        private LNPersonal _ln;
 
         
-        public FormDocumentoMasLeido(Documento doc)
+        public FormDocumentoMasLeido(LNPersonal ln)
         {
             InitializeComponent();
-            _doc = doc;
+            _ln = ln;
         }
 
         private void FormDocumentoMasLeido_Load(object sender, EventArgs e)
         {
-            if (_doc != null)
+            try
             {
-                txtTitulo.Text = _doc.Titulo;
-                txtAutor.Text = _doc.Autor;
-                txtEditorial.Text = _doc.Editorial;
-                txtIsbn.Text = _doc.Isbn;
+               LNAdquisiciones logicaAdq = _ln as LNAdquisiciones;
+
+                if (logicaAdq == null)
+                {
+                    MessageBox.Show("Error: Se requiere acceso de Adquisiciones para ver esta estadística.");
+                    this.Close();
+                    return;
+                }
+
+                 Documento ganador = logicaAdq.GetDocumentoMasPrestadoUltimoMes();
+
+                if (ganador != null)
+                {
+                    
+                    txtTitulo.Text = ganador.Titulo;
+                    txtAutor.Text = ganador.Autor;
+                    txtEditorial.Text = ganador.Editorial;
+                    txtIsbn.Text = ganador.Isbn;
+                }
+                else
+                {
+                     MessageBox.Show("No se han realizado préstamos en el último mes.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                
-                MessageBox.Show("No hay datos del documento.");
+                MessageBox.Show("Ocurrió un error al calcular estadísticas: " + ex.Message);
                 this.Close();
             }
         }
